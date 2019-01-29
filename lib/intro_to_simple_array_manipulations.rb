ae189b6364a2d97b2fc107b10bb3723431a5e27a
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-29 23:40:29 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -37,6 +37,6 @@ def using_concat(array, array1)
   array.concat(array1)
 end
 
-def using_insert(array, array1)
-  array.insert(3, "Malbolge")
+def using_insert(array, array1 = "Malbolge")
+  array.insert(3, array1)
 end
