c682f03078bf8e7ec1c6d10c2fda2de6f94accd6
Kevin Lopez Cifuentes <us.kevin@icloud.com>
2019-01-30 00:08:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -48,6 +48,6 @@ end
 
 def using_flatten(array)
   array = "New York University", "Manhattan School of Music ", "Columbia University", "The Juilliard School", "Bard College", "Cooper Union"
-  array.flatten()
+  array.flatten
   
 end
